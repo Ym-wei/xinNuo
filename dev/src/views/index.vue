65247bbeddfe9bc2f458c9b2eeb9a1d9b9586fcf
mingwei <285133472>
2021-08-03 14:11:31 +0800

    feat: build2

---
+++ dev/src/views/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    容器
+    <img src="https://cdn4.buysellads.net/uu/1/87530/1621530199-20210511_Ad-refresh-for-Carbon_Ads_01-_1_.png"
+         alt=""
+    >
+
+    <img src="https://thumbnail0.baidupcs.com/thumbnail/f3838586eu03fe2696dede636ce77b04?fid=1007908485-250528-401471721691768&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-mMEHy3Rq97D%2fyOJPgiMZ%2bNPaYmQ%3d&expires=8h&chkbd=0&chkv=0&dp-logid=365206973744773565&dp-callid=0&time=1627970400&size=c1920_u1080&quality=90&vuk=1007908485&ft=image&autopolicy=1"
+         alt=""
+    >
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    inheritAttrs: false,
+    components: {
+    },
+    data() {
+      return {
+
+      }
+    },
+
+    created() {
+      document.title = '杨昕诺'
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="scss">
+  .container {
+    background: #aaa;
+  }
+</style>
