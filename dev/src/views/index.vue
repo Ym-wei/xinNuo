6c534f4842d148ac00316665a07450439b738968
mingwei <285133472>
2021-08-04 14:21:47 +0800

    feat: 静态图片, 视频测试

--- dev/src/views/index.vue
+++ dev/src/views/index.vue
@@ -8,6 +8,10 @@
     <img src="https://thumbnail0.baidupcs.com/thumbnail/f3838586eu03fe2696dede636ce77b04?fid=1007908485-250528-401471721691768&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-mMEHy3Rq97D%2fyOJPgiMZ%2bNPaYmQ%3d&expires=8h&chkbd=0&chkv=0&dp-logid=365206973744773565&dp-callid=0&time=1627970400&size=c1920_u1080&quality=90&vuk=1007908485&ft=image&autopolicy=1"
          alt=""
     >
+    <img src="~@/assets/images/123.jpg"
+         alt=""
+    >
+    <video src="@/assets/mp4/mda-mh21ib7ehkey1nj9.mp4" muted controls="controls" loop="loop" autoplay="autoplay" ></video>
   </div>
 </template>
 
