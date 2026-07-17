5fa32f4c15871f5e6125dd86be06754693a04d9e
杨明炜 <285133472@qq.com>
2026-07-17 16:05:02 +0800

    feat: test

--- dev/src/router/index.js
+++ dev/src/router/index.js
@@ -1,14 +1,8 @@
-/*
- * @Author: zhongzhixin
- * @Date: 2023-02-07 11:18:21
- * @LastEditors: zhongzhixin
- * @LastEditTime: 2023-03-22 11:55:46
- */
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import PageRouter from './page'
 
 const router = createRouter({
-  history: createWebHistory('/xinNuo/'),
+  history: createWebHashHistory('/xinNuo/'),
   routes: [...PageRouter]
 })
 
@@ -16,4 +10,5 @@ router.beforeEach((to, from, next) => {
   document.title = to.meta.title || '杨昕诺'
   next()
 })
+
 export default router
--- dev/src/views/index/index.vue
+++ dev/src/views/index/index.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="wrapper">
     1
+
+    <div @click="jump">跳转</div>
   </div>
 </template>
 <script setup>
+import router from "@/router";
 
+const jump = () => {
+  router.push({
+    name: 'synopsisList'
+  })
+}
 </script>
 <style lang="scss" scoped>
 
